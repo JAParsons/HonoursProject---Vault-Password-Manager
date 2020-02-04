e0ea5158099f8af50a5b6dd7516c7a558c98db7f
JAParsons <johnparsons932@gmail.com>
2020-02-04 16:28:34 +0000

    Bug fixes
    - patched js library imports
    - webcam access now working again after serving content via https

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,11 +12,13 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src={{asset('js/jQuery.js')}}></script>
-    <script src={{asset('js/notify.js')}}></script>
+{{--    <script src={{asset('js/notify.js')}}></script>--}}
 
     <!-- TRYING TO GET PRELOADER TO WORK -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+
+    <script src={{asset('js/notify.js')}}></script>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 
@@ -123,6 +125,7 @@
 </footer>
 </body>
 
+{{--preloader div--}}
 <div class="se-pre-con"></div>
 
 </html>
